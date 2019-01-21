2e264975d7ac4fdc7bf8139cf82c15b8fa6869b5
chenqingsen <chenqingsen@96900.com.cn>
2019-01-21 13:41:12 +0800

    初始化项目

---
+++ Cs55App/Cs55Jobs/JobsService.cs
@@ -0,0 +1,50 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace Cs55Jobs
+{
+    public class JobsService:IJobsService
+    {
+        private static IScheduler scheduler;
+
+        private static async Task RunProgram()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+
+        public void Start()
+        {
+            RunProgram().GetAwaiter().GetResult();
+        }
+
+        public IScheduler Scheduler
+        {
+            get { return scheduler; }
+        }
+    }
+}
